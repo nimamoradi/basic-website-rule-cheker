b8ac12064da795359498bc6d064c54578f875d94
nima.moradi <ni_moradi@yahoo.com>
2019-06-30 01:01:04 +0430

    rule 1935 patch

--- rule/Rule1935.py
+++ rule/Rule1935.py
@@ -8,7 +8,9 @@ class Rule1935(Rule):
         # defined in CSS classes. Consolidating all styling into classes makes it easier to read, understand and
         # maintain.
 
-        style_tags = self.driver.find_elements_by_tag_name("style")
-        assert len(style_tags) == 0, 'style tag should not exist in html'
+        style_tags = self.driver.find_elements_by_xpath("//*[not(*)]")
+        for style_tag in style_tags:
+            style_att = style_tag.get_attribute("style")
+            assert style_att is None, 'style attribute should not exist in html'
 
         print('test passed 1935')
